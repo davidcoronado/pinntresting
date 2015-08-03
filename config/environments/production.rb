6ec41507cc8cc3c024e47f47dc4956576d52931f
davidcoronado <davidjcoronado@gmail.com>
2015-08-03 15:57:53 -0700

    Add Amazon S3 for Paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,16 @@ Rails.application.configure do
   # Set to actual Host
   config.action_mailer.default_url_options = { :host => 'gitdelivery.herokuapp.com' }
 
+  #Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
